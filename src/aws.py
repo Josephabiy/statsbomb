8a5e20547b25a1e798fc315f0834f0a8cd1f5ef4
Joseph Abiy <josephabiy@macbook-air.home>
2020-11-25 00:58:08 +0000

    etl taxi task

--- src/aws.py
+++ src/aws.py
@@ -11,8 +11,13 @@ def pull(csv_url):
             obj_name (str):  Object name
     Return:
     """
-    req = requests.get(csv_url)
-    url_content = req.content.decode("utf-8").split("\n")
-    rows = [row.split(",") for row in url_content]
-    rows = rows[1:]
-    return rows
+    try:
+        req = requests.get(csv_url)
+        url_content = req.content.decode("utf-8").split("\n")
+        rows = [row.split(",") for row in url_content]
+        rows = rows[1:]
+        return rows
+    except Exception as e:
+        raise ValueError(
+            f"An error has occurred whilst pulling '{csv_url}' from bucket AWS"
+        )
